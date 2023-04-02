e3ecc17c5cd6aa23004226467e3edebcc1ccf4f7
Maksym-Palii <paliym_@ukr.net>
2023-04-02 20:33:01 +0300

    bug fix package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 
