98bb9cc6b6e15fb644f02ed31064df5cc6555ab6
Maksym-Palii <paliym_@ukr.net>
2023-04-03 09:17:07 +0300

    bug fix NavLink

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,26 +1,35 @@
 import { Suspense } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import { Outlet, NavLink } from 'react-router-dom';
 import css from 'components/SharedLayout/SharedLayout.module.css';
 
-const StyledLink = styled(NavLink)`
-  &.active {
-    color: red;
-  }
-`;
+// const StyledLink = styled(NavLink)`
+//   &.active {
+//     color: red;
+//   }
+// `;
 
 const SharedLayout = () => {
   return (
     <div>
       <header className={css.header}>
         <nav>
+          <NavLink className={css.navLink} to="/" end>
+            Home
+          </NavLink>
+          <NavLink className={css.navLink} to="/movies">
+            Movies
+          </NavLink>
+        </nav>
+
+        {/* <nav>
           <StyledLink className={css.navLink} to="/" end>
             Home
           </StyledLink>
           <StyledLink className={css.navLink} to="/movies">
             Movies
           </StyledLink>
-        </nav>
+        </nav> */}
       </header>
       <main>
         <Suspense fallback={<div>Loading...</div>}>
